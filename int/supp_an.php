1c2d12326683707e4573e7c7ef6b05855772fb03
SaharSs <ssinfotech762@gmail.com>
2020-10-13 05:26:41 +0200

    changement1

--- int/supp_an.php
+++ int/supp_an.php
@@ -6,9 +6,25 @@ header('location:login_user.php');
 if(isset($_GET['id']))
 {
 $id=$_GET['id'];
-
- $q="delete from annonce where id='$id'";
-$r=mysqli_query($c,$q);
+$t="select image from image_an where i_id=".$id;
+$h=mysqli_query($c,$t);
+$p=mysqli_num_rows($h);
+    if($p>0)
+        {
+            
+          while($row=mysqli_fetch_assoc($h))
+          {
+              if (is_file('images/avatar1/'.$row['image'])) 
+              
+               unlink('images/avatar1/'.$row['image']);
+         
+          }
+        }
+     
+ $m="delete from annonce where id='$id'";
+        $r1=mysqli_query($c,$m);   
     
+    
+     
 header('location:index.html');   
 }
