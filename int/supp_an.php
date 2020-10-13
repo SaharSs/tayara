1381c72b778811a69b25313aef35d4b6324d7177
codeurx-amir <compte.bzarba@gmail.com>
2020-10-13 22:35:11 +0200

    supp

--- int/supp_an.php
+++ int/supp_an.php
@@ -5,10 +5,20 @@ if(!isset($_SESSION['user']))
 header('location:login_user.php');
 if(isset($_GET['id']))
 {
-$id=$_GET['id'];
-
- $q="delete from annonce where id='$id'";
-$r=mysqli_query($c,$q);
-    
-header('location:index.html');   
+    $id=$_GET['id'];
+    $imgs = "SELECT * FROM image_an WHERE i_id='$id'";
+    $query_imgs = mysqli_query($c,$imgs);
+    $count_query_imgs = mysqli_num_rows($query_imgs);
+    if($count_query_imgs>0){
+        while($res_query_imgs = mysqli_fetch_assoc($query_imgs)){
+            $res_query_imgs['image'];
+            unlink('images/avatar1/'.$res_query_imgs['image']);
+        }
+        $q="delete from annonce where id='$id'";
+        $r=mysqli_query($c,$q);
+    }else{
+        $q="delete from annonce where id='$id'";
+        $r=mysqli_query($c,$q);
+    }
+    //header('location:index.html');
 }
